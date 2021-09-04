43dfd6f529739021d638154661ab665aa7a796d8
Shivani Vemula <76956616+shivanidotcom@users.noreply.github.com>
2021-09-05 00:38:44 +0530

    Add files via upload

---
+++ Animations.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import './Animations.css';
+const Animations = () => {
+    return (
+        <div>
+            
+                <body>
+                    <div class="container">
+                        <div class="animations"> 
+                        </div>
+                        <div class="animations"> 
+                        </div>
+                        <div class="animations"> 
+                        </div>
+                        <div class="animations"> 
+                        </div>
+                        <div class="animations"> 
+                        </div>
+                    </div>
+                    <div class="container">
+                        <div class="animations"> 
+                        </div>
+                        <div class="animations"> 
+                        </div>
+                        <div class="animations"> 
+                        </div>
+                        <div class="animations"> 
+                        </div>
+                        <div class="animations"> 
+                        </div>
+                    </div>
+                    <div class="container">
+                        <div class="animations"> 
+                        </div>
+                        <div class="animations"> 
+                        </div>
+                        <div class="animations"> 
+                        </div>
+                        <div class="animations"> 
+                        </div>
+                        <div class="animations"> 
+                        </div>
+                    </div>
+                    <div class="container">
+                        <div class="animations"> 
+                        </div>
+                        <div class="animations"> 
+                        </div>
+                        <div class="animations"> 
+                        </div>
+                        <div class="animations"> 
+                        </div>
+                        <div class="animations"> 
+                        </div>
+                    </div>
+                    <div class="container">
+                        <div class="animations"> 
+                        </div>
+                        <div class="animations"> 
+                        </div>
+                        <div class="animations"> 
+                        </div>
+                        <div class="animations"> 
+                        </div>
+                        <div class="animations"> 
+                        </div>
+                    </div>
+                    <div class="container">
+                        <div class="animations"> 
+                        </div>
+                        <div class="animations"> 
+                        </div>
+                        <div class="animations"> 
+                        </div>
+                        <div class="animations"> 
+                        </div>
+                        <div class="animations"> 
+                        </div>
+                    </div>
+                    
+                </body>
+
+        </div>
+    );
+};
+
+export default Animations;
